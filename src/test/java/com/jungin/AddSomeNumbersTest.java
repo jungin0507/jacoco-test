0e1e347da3d47829f15be838e2c4146531790677
jungin0507 <a01033046758@gmail.com>
2022-08-01 23:44:36 +0900

    docs: README.md

---
+++ src/test/java/com/jungin/AddSomeNumbersTest.java
@@ -0,0 +1,22 @@
+package com.jungin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddSomeNumbersTest {
+
+    @Test
+    int addTwoNumbers() {
+        AddSomeNumbers addSomeNumbers = new AddSomeNumbers();
+        int result = addSomeNumbers.addTwoNumbers(50, 30);
+        assertEquals(80, result);
+        return result;
+    }
+
+    @Test
+    void shouldPrintSumToConsole(){
+        AddSomeNumbers.main(new String[] {});
+        assertEquals(addTwoNumbers(), 80);
+    }
+}
